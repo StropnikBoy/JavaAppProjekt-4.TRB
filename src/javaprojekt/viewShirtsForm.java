a85c4034e6484758c849d4397a9b34ff8a48bca5
StropnikBoy <thephonex23@gmail.com>
2018-05-13 18:11:42 +0200

    User Store progress

--- src/javaprojekt/viewShirtsForm.java
+++ src/javaprojekt/viewShirtsForm.java
@@ -35,6 +35,8 @@ public class viewShirtsForm extends javax.swing.JFrame {
         jCancelButton = new javax.swing.JButton();
         jBrandLabel = new javax.swing.JLabel();
         jSizesComboBox = new javax.swing.JComboBox<>();
+        jTypeLabel = new javax.swing.JLabel();
+        jTypeComboBox = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -52,7 +54,7 @@ public class viewShirtsForm extends javax.swing.JFrame {
         jSizesLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jSizesLabel.setText("Sizes:");
         jPanel1.add(jSizesLabel);
-        jSizesLabel.setBounds(20, 190, 61, 29);
+        jSizesLabel.setBounds(30, 260, 61, 29);
 
         jNameComboBox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jNameComboBox.addActionListener(new java.awt.event.ActionListener() {
@@ -61,7 +63,7 @@ public class viewShirtsForm extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jNameComboBox);
-        jNameComboBox.setBounds(120, 100, 197, 50);
+        jNameComboBox.setBounds(120, 190, 197, 50);
 
         jBrandComboBox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jBrandComboBox.addActionListener(new java.awt.event.ActionListener() {
@@ -70,22 +72,22 @@ public class viewShirtsForm extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jBrandComboBox);
-        jBrandComboBox.setBounds(120, 20, 197, 50);
+        jBrandComboBox.setBounds(120, 130, 197, 50);
 
         jFemaleRadioButton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jFemaleRadioButton.setText("Female");
         jPanel1.add(jFemaleRadioButton);
-        jFemaleRadioButton.setBounds(110, 290, 83, 31);
+        jFemaleRadioButton.setBounds(120, 340, 83, 31);
 
         jMaleRadioButton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jMaleRadioButton.setText("Male");
         jPanel1.add(jMaleRadioButton);
-        jMaleRadioButton.setBounds(240, 290, 65, 31);
+        jMaleRadioButton.setBounds(240, 340, 65, 31);
 
         jNameLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jNameLabel.setText("Name:");
         jPanel1.add(jNameLabel);
-        jNameLabel.setBounds(20, 110, 70, 29);
+        jNameLabel.setBounds(30, 200, 70, 29);
 
         jCheckoutButton.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jCheckoutButton.setText("Checkout");
@@ -110,7 +112,7 @@ public class viewShirtsForm extends javax.swing.JFrame {
         jBrandLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jBrandLabel.setText("Brand:");
         jPanel1.add(jBrandLabel);
-        jBrandLabel.setBounds(20, 30, 70, 29);
+        jBrandLabel.setBounds(30, 140, 70, 29);
 
         jSizesComboBox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jSizesComboBox.addActionListener(new java.awt.event.ActionListener() {
@@ -119,16 +121,40 @@ public class viewShirtsForm extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jSizesComboBox);
-        jSizesComboBox.setBounds(120, 190, 197, 50);
+        jSizesComboBox.setBounds(120, 250, 197, 50);
+
+        jTypeLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jTypeLabel.setText("Type:");
+        jPanel1.add(jTypeLabel);
+        jTypeLabel.setBounds(30, 80, 60, 29);
+
+        jTypeComboBox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jTypeComboBox.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                jTypeComboBoxItemStateChanged(evt);
+            }
+        });
+        jTypeComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTypeComboBoxActionPerformed(evt);
+            }
+        });
+        jTypeComboBox.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                jTypeComboBoxPropertyChange(evt);
+            }
+        });
+        jPanel1.add(jTypeComboBox);
+        jTypeComboBox.setBounds(120, 70, 197, 50);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 816, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -161,47 +187,91 @@ public class viewShirtsForm extends javax.swing.JFrame {
     }//GEN-LAST:event_jBrandComboBoxActionPerformed
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
-        jBrandComboBox.addItem("Maverick");
-        jBrandComboBox.addItem("Adidas");
+        Connection Conn;
+        Database povezava = new Database();
+        Conn = povezava.getConnection();
         
-        jNameComboBox.addItem("S");
-        jNameComboBox.addItem("M");
-        jNameComboBox.addItem("L");
-        jNameComboBox.addItem("XL");
+        Integer tempCategory = null;
+        
+        try 
+        {
+            Statement stmt = Conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM categories");
+        
+            while (rs.next()) {
+                String pat = rs.getString("name");
+                jTypeComboBox.addItem(pat);
+                tempCategory = Integer.parseInt(pat);
+            }
+            Conn.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+        try 
+        {
+            Statement stmt = Conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+        
+            while (rs.next()) {
+                String pat = rs.getString("name");
+                jBrandComboBox.addItem(pat);
+            }
+            Conn.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+          
+        jSizesComboBox.addItem("S");
+        jSizesComboBox.addItem("M");
+        jSizesComboBox.addItem("L");
+        jSizesComboBox.addItem("XL");
+        
+        
     }//GEN-LAST:event_formWindowOpened
 
     private void jSizesComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jSizesComboBoxActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jSizesComboBoxActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(viewShirtsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(viewShirtsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(viewShirtsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(viewShirtsForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jTypeComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTypeComboBoxActionPerformed
+        jNameComboBox.addItem("Niggers");
+    }//GEN-LAST:event_jTypeComboBoxActionPerformed
 
-        /* Create and display the form */
+    private void jTypeComboBoxPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jTypeComboBoxPropertyChange
+ 
+    }//GEN-LAST:event_jTypeComboBoxPropertyChange
+
+    private void jTypeComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jTypeComboBoxItemStateChanged
+        Connection Conn;
+        Database povezava = new Database();
+        Conn = povezava.getConnection();
+        
+        String value = jTypeComboBox.getSelectedItem().toString();
+        String sql = "SELECT id FROM categories WHERE name = '" + value + "'";
+        Integer id = Integer.parseInt(sql);
+
+        
+        try 
+        {
+            Statement stmt = Conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads WHERE category_id = " + id +"");
+        
+            while (rs.next()) {
+                String pat = rs.getString("name");
+                jBrandComboBox.addItem(pat);
+            }
+            Conn.close();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jTypeComboBoxItemStateChanged
+
+    public static void main(String args[]) {
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new viewShirtsForm().setVisible(true);
@@ -222,5 +292,7 @@ public class viewShirtsForm extends javax.swing.JFrame {
     private javax.swing.JLabel jPictureBox;
     private javax.swing.JComboBox<String> jSizesComboBox;
     private javax.swing.JLabel jSizesLabel;
+    private javax.swing.JComboBox<String> jTypeComboBox;
+    private javax.swing.JLabel jTypeLabel;
     // End of variables declaration//GEN-END:variables
 }
