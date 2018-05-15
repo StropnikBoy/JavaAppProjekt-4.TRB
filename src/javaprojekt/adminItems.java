d6e44f2acdf77131a19493309d5decfa361bfc5f
StropnikBoy <thephonex23@gmail.com>
2018-05-15 03:28:07 +0200

    well it works :P

--- src/javaprojekt/adminItems.java
+++ src/javaprojekt/adminItems.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaprojekt;
-
 import java.awt.Toolkit;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -274,7 +268,7 @@ public class adminItems extends javax.swing.JFrame {
         try
         {
             Statement stavek = Conn.createStatement();
-            String query = "SELECT INSERT INTO pictures (ad_id, name) VALUES (
+            String query = "SELECT INSERT INTO pictures (ad_id, name) VALUES (";
             System.out.println(query);
             Statement statement = Conn.createStatement();
             statement.executeQuery(query);
@@ -290,34 +284,7 @@ public class adminItems extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jAddButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(adminItems.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(adminItems.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(adminItems.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(adminItems.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new adminItems().setVisible(true);
